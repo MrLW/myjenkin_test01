9430857ba7ae7a95fac88bcc081156fe152f0be6
18321844291 <wen.li@unicareer.com>
2020-02-14 12:04:31 +0800

    feat: init

---
+++ index.js
@@ -0,0 +1,14 @@
+/*
+ * @Description: Jenkins Demo
+ * @Author: leekwe
+ * @Date: 2020-02-14 12:00:52
+ * @version: 1.0
+ * @LastEditors  : leekwe
+ * @LastEditTime : 2020-02-14 12:02:46
+ */
+const http = require('http');
+const app = http.createServer((req,res)=>{
+    res.end("Hello, Jenkins, from v1.0.0");
+});
+console.info('app is running on 3000');
+app.listen(3000);
